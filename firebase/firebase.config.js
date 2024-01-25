e22adb177bdd2719f9134eb4e37497166b042eb0
Ian <kernohani1@gmail.com>
2024-01-24 21:02:37 -0500

    github pages

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 
 
 
